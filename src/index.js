f4e5776a0c935672af80dee790693e366833822f
Hemant kumar <hemantkumarsonkar1@gmail.com>
2024-05-27 22:21:06 +0530

    update the content of this file.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ const fileName = 'myfile.txt';
 const updateFile = async (fileName, fileContent) => {
     //Write your code here to overwrite the file content
     //Don't change function name
-    
+    await fs.writeFile(fileName, fileContent);
+
 };
+
+updateFile(fileName, "Newton School, is an online learning platform second porson");
 
 
 module.exports = updateFile;
